25885f93dd6dbeaa48845cb613fb8f87179c29f3
KARTIK10112007 <77154878+KARTIK10112007@users.noreply.github.com>
2021-09-17 09:45:24 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
-var bg = "sprites/bg1.png";
+var bg = "sprites/bg.png";
 var score = 0;
 
 function preload() {
@@ -109,7 +109,7 @@ async function getBackgroundImg(){
     var hour = datetime.slice(11,13);
     
     if(hour>=0600 && hour<=1900){
-        bg = "sprites/bg1.png";
+        bg = "sprites/bg.png";
     }
     else{
         bg = "sprites/bg2.jpg";
